4ab0b2954a43a0c503316f94d211906ed1c5abca
BineshNarayanan <kbinesh@gmail.com>
2018-05-15 21:41:21 +0530

    App Skeleton

---
+++ Source/ReferralAPI/referral-services-api/referral-services-controller/src/main/java/com/referral/starter/Application.java
@@ -0,0 +1,23 @@
+package com.referral.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"com.referral.*"})
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
